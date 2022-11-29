abfa83c1ebe134f25ae8aec684f9d5abb4cf8571
Braian Silva <braiannogueirasilva@gmail.com>
2022-11-28 21:39:24 -0300

    Update Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,6 +7,43 @@ export function Header() {
     const [nav, setNave] = useState(false);
     const barToggle = () => setNave(!nav);
 
+    const navItems = [
+        {
+            name: "Home.",
+            link: "hero"
+        },
+        {
+            name: "Portfolio.",
+            link: "portfolio"
+        },
+        {
+            name: "Skill.",
+            link: "skill"
+        },
+        {
+            name: "About.",
+            link: "about"
+        },
+        {
+            name: "Contact.",
+            link: "contact"
+        }
+    ]
+
+    function getNavItems() {
+        return navItems.map(item =>
+            <Link activeClass="nav--active" className="nav__item p-3" smooth spy to={item.link}>{item.name}</Link>
+        );
+    }
+
+    function getBurgerItems() {
+        return navItems.map(item =>
+            <li>
+                <Link activeClass="nav--active" className="nav__item" smooth spy to={item.link}>{item.name}</Link>
+            </li>
+        );
+    }
+
     return (
         <div className="w-screen h-[80px] z-10 bg-container fixed drop-shadow-lg">
             <div className="px-2 nav flex justify-between items-center w-full h-full">
@@ -16,11 +53,9 @@ export function Header() {
                 </div>
                 <div className="hidden md:flex pr-2">
                     <ul className="text-1xl font-semibold hidden md:flex title">
-                        <Link activeClass="nav--active" className="nav__item p-3" smooth spy to="hero">Home.</Link>
-                        <Link activeClass="nav--active" className="nav__item p-3" smooth spy to="portfolio">Portfolio.</Link>
-                        <Link activeClass="nav--active" className="nav__item p-3" smooth spy to="skill">Skill.</Link>
-                        <Link activeClass="nav--active" className="nav__item p-3" smooth spy to="contact">Contact.</Link>
-                        <Link activeClass="nav--active" className="nav__item p-3 pr-10" smooth spy to="about">About.</Link>
+                        {
+                            getNavItems()
+                        }
                     </ul>
                 </div>
 
@@ -31,11 +66,9 @@ export function Header() {
             </div>
 
             <ul className={!nav ? 'hidden ' : 'absolute bg-container w-full px-8 md:hidden'}>
-                <li> <Link activeClass="nav--active" smooth spy to="hero">Home</Link></li>
-                <li> <Link activeClass="nav--active" smooth spy to="portfolio">Portfolio</Link></li>
-                <li> <Link activeClass="nav--active" smooth spy to="skill">Skill</Link></li>
-                <li> <Link activeClass="nav--active" smooth spy to="contact">Contact</Link></li>
-                <li> <Link activeClass="nav--active" smooth spy to="About">About</Link></li>
+                {
+                    getBurgerItems()
+                }
             </ul>
         </div>
     )
