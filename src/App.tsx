b409686db9a7a344ec5903a4bf45322852740209
Braian Silva <braiannogueirasilva@gmail.com>
2022-11-28 21:12:56 -0300

    Remove App css import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 import "./assets/sass/_index.scss";
-import './App.css'
 import Layout from './components/layout/Layout'
 import Footer from './components/layout/Footer'
 
