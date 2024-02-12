6467317554a34260904b23dad2f74c8d67e79889
BraianS <braiannogueirasilva@gmail.com>
2024-02-12 11:20:03 -0300

    Update Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 function Footer() {
   return (
     <div className="flex justify-center items-center w-full h-[100px] bg-red-600 text-white">
-      <p className="font-bold">© 2022 ALL RIGHT RESEVED</p>
+      <p className="font-bold">© 2024 Braian Silva. Este site é disponibilizado sob a Licença MIT.</p>
     </div>
   )
 }
